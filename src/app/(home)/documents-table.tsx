350cdf9b1299a8a57c8c225b27ab035cca16e3b5
Ansh <ansh2shweta@gmail.com>
2025-07-22 18:30:00 +0530

    docs rendered on home page

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -9,6 +9,8 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import { LoaderIcon } from "lucide-react";
+import { DocumentRow } from "./document-row";
 
 interface DocumentsTableProps {
     documents : Doc<"documents">[] | undefined
@@ -23,13 +25,38 @@ export const DocumentsTable = ({
     status
 }: DocumentsTableProps) =>{
     return (
-        <div className="max-w-screen-xl max-auto px-16 py-6 flex flex-col gap-5">
-            {document == undefined ? (
-                <div>
-                    
+        <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5">
+            {documents == undefined ? (
+                <div className="flex justify-center items-center h-24">
+
+                    <LoaderIcon className="animate-spin text-muted-foreground size-8"/>
                 </div>
             ):(
-
+                <Table>
+                    <TableHeader>
+                        <TableRow className="hover:bg-transparent border-none">
+                            <TableHead>Name</TableHead>
+                            <TableHead>&nbsp;</TableHead>
+                            <TableHead className="hidden md:table-cell">Shared</TableHead>
+                            <TableHead className="hidden md:table-cell">Created At</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    {documents?.length == 0 ? (
+                        <TableBody>
+                            <TableRow className="hover:bg-transparent">
+                                <TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
+                                    No Document Found
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    ):(
+                        <TableBody>
+                            {documents.map((document)=>(
+                                <DocumentRow key={document._id} document = {document} />
+                            ))}
+                        </TableBody>
+                    )}
+                </Table>
             )}
         </div>
     )
